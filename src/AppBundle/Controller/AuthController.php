1f0e9531dc386a49d71f53757f3d935841c520b5
ehsan <ehsan.khf@gmail.com>
2017-08-28 17:13:38 +0100

    [backend]: auth ctrl is structured

--- src/AppBundle/Controller/AuthController.php
+++ src/AppBundle/Controller/AuthController.php
@@ -7,12 +7,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AuthController extends Controller
 {
+    //TODO: make it only POST
     /**
      * @Route("/api/version1/user/login", name="login")
-     * @Method({"POST"})
+     * @Method({"POST","GET"})
      */
     public function loginAction(Request $request)
     {
@@ -23,18 +26,24 @@ class AuthController extends Controller
         //TODO: Check the user
         //TODO: Cookie creation
         //TODO: response
-
+        $response = new JsonResponse();
+        $response->setData(array('status' => 1));
+        return $response;
     }
 
+    //TODO: make it only POST
     /**
      * @Route("/api/version1/user/logout", name="logout")
-     * @Method({"POST"})
+     * @Method({"POST","GET"})
      */
     public function logoutAction(Request $request)
     {
         $userid = $request->request->get('userid');
         $em = $this->getDoctrine()->getManager();
         //TODO: validation
+        $response = new JsonResponse();
+        $response->setData(array('status' => 1));
+        return $response;
 
     }
 
