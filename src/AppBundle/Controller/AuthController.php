f67dfd486e81ab7f8150f2169925e9ba245d5ada
ehsan <ehsan.khf@gmail.com>
2017-08-28 17:32:20 +0100

    [backed]: the user model is created and the login is still in progress

--- src/AppBundle/Controller/AuthController.php
+++ src/AppBundle/Controller/AuthController.php
@@ -10,6 +10,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
+use AppBundle\Entity\Users;
+
 class AuthController extends Controller
 {
     //TODO: make it only POST
@@ -19,11 +21,56 @@ class AuthController extends Controller
      */
     public function loginAction(Request $request)
     {
-        $username = $request->request->get('username');
-        $password = $request->request->get('password');
+        //TODO: make it only POST
+        if ($request->isMethod('GET')) {
+            $username = $request->query->get('username');
+            $password = $request->query->get('password');
+        } else {
+            $username = $request->request->get('username');
+            $password = $request->request->get('password');
+        }
 
-        //TODO: validation
-        //TODO: Check the user
+        //DONE: validation
+        if (
+            (empty($password) || strlen(trim($password)) < 8 || strlen(trim($password)) > 15) ||
+            (empty($username) || strlen(trim($username)) < 2 || strlen(trim($username)) > 30)
+        ) {
+            $response = new JsonResponse();
+            $response->setData(array('status' => 0, 'message' => 'invalid user information'));
+            return $response;
+        }
+
+        //DONE: Check the user
+        $user = $this->getDoctrine()
+            ->getRepository('AppBundle:Users')
+            ->findOneByUsername($username);
+        
+        if (empty($user)) {
+            // the user is getting created if it doesn't exist
+            $user = new Users();
+
+            $options = [
+                'cost' => 12,
+            ];
+            $password = password_hash($password, PASSWORD_BCRYPT, $options);
+
+            $user->setPassword($password);
+
+            $user->setUsername($username);
+
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($user);
+            $em->flush();
+        } else {
+            // if the user exist it will be checked against the stored password
+            if (!password_verify($password, $user->getPassword())) {
+                $response = new JsonResponse();
+                $response->setData(array('status' => 0, 'message' => 'wrong password'));
+                return $response;
+            }
+        }
+
         //TODO: Cookie creation
         //TODO: response
         $response = new JsonResponse();
